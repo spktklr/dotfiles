acca66735390be42944a4609f89c9d436d106468
Niko Myllynen <nmyllynen@gmail.com>
2016-04-27 10:40:55 +0300

    fix YCMinstall

--- scripts/YouCompleteMe.sh
+++ scripts/YouCompleteMe.sh
@@ -4,7 +4,7 @@ rm -rf ycm_build
 mkdir ycm_build
 cd ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-cmake --build . --target ycm_support_libs --config Release
+cmake --build . --target ycm_core --config Release
 cd /Users/niko/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/tern_runtime
 npm i --production
 echo "Done."
